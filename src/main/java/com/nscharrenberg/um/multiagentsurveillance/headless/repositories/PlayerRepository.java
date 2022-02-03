d72db87592eff71004a1c0740f74e81c4b6af403
Noah <nscharrenberg@hotmail.com>
2022-02-03 21:32:52 +0100

    Basic player Repository added

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/repositories/PlayerRepository.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/repositories/PlayerRepository.java
@@ -1,4 +1,33 @@
 package com.nscharrenberg.um.multiagentsurveillance.headless.repositories;
 
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Guard;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Intruder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerRepository {
+    private List<Intruder> intruders;
+    private List<Guard> guards;
+
+    public PlayerRepository() {
+        this.intruders = new ArrayList<>();
+        this.guards = new ArrayList<>();
+    }
+
+    public List<Intruder> getIntruders() {
+        return intruders;
+    }
+
+    public void setIntruders(List<Intruder> intruders) {
+        this.intruders = intruders;
+    }
+
+    public List<Guard> getGuards() {
+        return guards;
+    }
+
+    public void setGuards(List<Guard> guards) {
+        this.guards = guards;
+    }
 }

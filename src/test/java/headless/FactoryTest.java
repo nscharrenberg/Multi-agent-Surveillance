d664fda856d25e7717e8e4e4e265b6def595ecc3
Noah <nscharrenberg@hotmail.com>
2022-02-03 21:58:12 +0100

    fix typo

--- src/test/java/headless/FactoryTest.java
+++ src/test/java/headless/FactoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public class FactoryTest {
 
-    @DisplayName("Factory Reset Succesful")
+    @DisplayName("Factory Reset Successful")
     @Test
     void testFactoryResetSuccess() {
         Factory.getGameRepository().setGameMode(GameMode.NO_COMMUNICATION);

977d81178b35f033984046af781f40ef6930d7f2
Noah <nscharrenberg@hotmail.com>
2022-02-07 14:57:19 +0100

    fix width/height

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/files/MapImporter.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/files/MapImporter.java
@@ -117,8 +117,8 @@ public class MapImporter {
     private void initTiles() {
         Factory.getMapRepository().getBoard().clear();
 
-        for (int i = 0; i <= Factory.getGameRepository().getHeight(); i++) {
-            for (int j = 0; j <= Factory.getGameRepository().getWidth(); j++) {
+        for (int i = 0; i <= Factory.getGameRepository().getWidth(); i++) {
+            for (int j = 0; j <= Factory.getGameRepository().getHeight(); j++) {
                 Factory.getMapRepository().getBoard().add(new Tile(i, j));
             }
         }

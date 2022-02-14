4f67d88c2cb5c8abe21057154a0c99ad09342d8e
Tjardo <44730725+Whatsoutside@users.noreply.github.com>
2022-02-15 00:37:45 +0100

    Implemented vision method

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/CharacterVision.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/CharacterVision.java
@@ -1,22 +1,23 @@
 package com.nscharrenberg.um.multiagentsurveillance.headless.utils;
 
-import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle;
-import com.nscharrenberg.um.multiagentsurveillance.headless.models.Area;
-import com.nscharrenberg.um.multiagentsurveillance.headless.models.Tile;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.*;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
 
 // probably have players inherit this class later
-public class CharacterVision {
+public class CharacterVision{
     private int length;
     private Angle direction;
+    private Geometrics gm;
 
     public CharacterVision(int length, Angle direction) {
         this.length = length;
         this.direction = direction;
+        gm = new Geometrics();
     }
 
-    // TODO: convert it from arraylist to area later on probably, waiting on NOAH's update on area
     public ArrayList<Tile> getRawVision(Tile position) {
         int px = position.getX();
         int py = position.getY();
@@ -24,7 +25,6 @@ public class CharacterVision {
         ArrayList<Tile> observation = new ArrayList<Tile>();
         observation.add(position);
 
-        // TODO: handle out of bounds tiles
         if(this.direction == Angle.DOWN) {
             for(int k=0; k < length; k++) {
                 for(int r=0; r < s; r++) {
@@ -56,31 +56,58 @@ public class CharacterVision {
                 }
                 s -= 2;
             }
-        } else {
-            // Invalid direction
         }
 
         return observation;
     }
 
-    // Ignore for now
-//    // TODO: properly rework method to check for item collision
-//    public ArrayList<Tile> getRealVision(ArrayList<Tile> rawvision) {
-//        ArrayList<Tile> finalvision = new ArrayList<>();
-//
-//        for (Tile t : rawvision) {
-//            // Trim out of bounds tiles
-//            if(t.getX() < 0 || t.getY() < 0 || t.getX() > 48 || t.getY() > 24) {
-//                //got to check map values
-//            }
-//            if(t.getItems().size() == 0) {
-//                // dummy method, will rework to proper item recognition later
-//                finalvision.add(t);
-//            }
-//        }
-//
-//        return finalvision;
-//    }
+
+    private ArrayList<Tile> getRealVision(TileArea board, ArrayList<Tile> rawvision, Tile position) {
+        ArrayList<Tile> finalvision = new ArrayList<>();
+        boolean validtile = true;
+
+        // Remove out of bound tiles first (maybe redundant if we use Optional)
+        rawvision.removeIf(tc -> (tc.getX() < 0 || tc.getY() < 0));
+        rawvision.removeIf(tc -> (tc.getX() > board.width() || tc.getY() > board.height()));
+
+        // Check remaining tiles for items
+        for (Tile t : rawvision) {
+            if (unobstructedTile(board, t)) {
+                for (Tile it : gm.getIntersectingTiles(position, t)) {
+                    if(!unobstructedTile(board,it)) {
+                        validtile = false;
+                        break;
+                    }
+                }
+            }
+
+            if(validtile)
+                finalvision.add(t);
+        }
+
+        return finalvision;
+    }
+
+    // TODO: Confirm if return parameter is what we want
+    public ArrayList<Tile> getVision(TileArea board, Tile position) {
+        return getRealVision(board, getRawVision(position), position);
+    }
+
+    private boolean unobstructedTile(TileArea board, Tile t) {
+        if(board.getByCoordinates(t.getX(), t.getY()).isPresent()) {
+            if (board.getByCoordinates(t.getX(), t.getY()).get().getItems().size() != 0) {
+                for (Item im : t.getItems()) {
+                    if (im instanceof Wall) {   // Might have to add other checks to this later
+                        return false;
+                    }
+                }
+            }
+        } else {
+            return false;
+        }
+
+        return true;
+    }
 
     public int getLength() {
         return length;

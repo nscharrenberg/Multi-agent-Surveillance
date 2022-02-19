95173116e7723baa1c574e77febdd6ccdd08dcd3
Noah Scharrenberg <nscharrenberg@hotmail.com>
2022-02-19 15:20:28 +0100

    stop on fully explored

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/gui/javafx/controllers/GameController.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/gui/javafx/controllers/GameController.java
@@ -1,24 +1,18 @@
 package com.nscharrenberg.um.multiagentsurveillance.gui.javafx.controllers;
 
-import com.nscharrenberg.um.multiagentsurveillance.agents.IAgent;
 import com.nscharrenberg.um.multiagentsurveillance.agents.random.RandomAgent;
+import com.nscharrenberg.um.multiagentsurveillance.agents.shared.Agent;
 import com.nscharrenberg.um.multiagentsurveillance.headless.Factory;
-import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Guard;
 import com.nscharrenberg.um.multiagentsurveillance.headless.utils.files.MapImporter;
-import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GameController {
-
-    private List<IAgent> agents = new ArrayList<>();
     private GameBoardGUI boardGUI;
 
     public GameController(){
@@ -77,11 +71,6 @@ public class GameController {
         for (int i = 0; i <= Factory.getGameRepository().getGuardCount(); i++) {
             Factory.getPlayerRepository().spawn(Guard.class);
         }
-
-        // TODO: spawn Intruders
-        for (Guard guard : Factory.getPlayerRepository().getGuards()) {
-            agents.add(new RandomAgent(guard));
-        }
     }
 
     private void gameLoop() {
@@ -89,22 +78,23 @@ public class GameController {
 
         Factory.getGameRepository().setRunning(true);
         while (Factory.getGameRepository().isRunning()) {
-            for (IAgent agent : agents) {
+            for (Agent agent : Factory.getPlayerRepository().getAgents()) {
                 agent.execute();
             }
 
             boardGUI.updateGUI();
 
+
+            // temp step count check
+            stepCount++;
+            if (stepCount >= 1000 || Factory.getPlayerRepository().getExplorationPercentage() >= 100) {
+                break;
+            }
+
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-
-            // temp step count check
-            stepCount++;
-            if (stepCount >= 1000) {
-                break;
             }
         }
     }

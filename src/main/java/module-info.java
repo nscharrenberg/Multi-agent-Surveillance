57a7bd1a34ba0621344bf0478f3c9adabdda750f
Noah Scharrenberg <nscharrenberg@hotmail.com>
2022-02-19 15:20:13 +0100

    modularity fixes?

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,16 @@
 module com.nscharrenberg.um.multiagentsurveillance {
     requires javafx.controls;
     requires javafx.fxml;
+    requires cloning;
 
-    opens com.nscharrenberg.um.multiagentsurveillance to javafx.fxml;
+    opens com.nscharrenberg.um.multiagentsurveillance to javafx.fxml, cloning;
     exports com.nscharrenberg.um.multiagentsurveillance;
     exports com.nscharrenberg.um.multiagentsurveillance.gui.javafx.controllers;
     opens com.nscharrenberg.um.multiagentsurveillance.gui.javafx.controllers to javafx.fxml;
+    exports com.nscharrenberg.um.multiagentsurveillance.headless.repositories;
+    opens com.nscharrenberg.um.multiagentsurveillance.headless.repositories to cloning;
+    exports com.nscharrenberg.um.multiagentsurveillance.headless.models;
+    opens com.nscharrenberg.um.multiagentsurveillance.headless.models to cloning;
+    opens com.nscharrenberg.um.multiagentsurveillance.agents.random to cloning;
+    opens com.nscharrenberg.um.multiagentsurveillance.agents.shared to cloning;
 }

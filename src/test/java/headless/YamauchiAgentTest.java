d4867d279e68e6295121e124b5e2d665deed51f1
Noah Scharrenberg <nscharrenberg@hotmail.com>
2022-02-26 17:39:26 +0100

    add Astar test

--- src/test/java/headless/YamauchiAgentTest.java
+++ src/test/java/headless/YamauchiAgentTest.java
@@ -1,5 +1,6 @@
 package headless;
 
+import com.nscharrenberg.um.multiagentsurveillance.agents.shared.algorithms.pathfinding.AStar.AStar;
 import com.nscharrenberg.um.multiagentsurveillance.agents.shared.utils.QueueNode;
 import com.nscharrenberg.um.multiagentsurveillance.agents.frontier.yamauchi.YamauchiAgent;
 import com.nscharrenberg.um.multiagentsurveillance.agents.shared.algorithms.pathfinding.BFS.BFS;
@@ -63,6 +64,64 @@ public class YamauchiAgentTest {
             Tile target = targetOpt.get();
 
             Optional<QueueNode> planDataOpt = (new BFS()).execute(agent.getKnowledge(), agent.getPlayer(), target);
+
+            if (planDataOpt.isEmpty()) {
+                Assertions.fail();
+            }
+
+            QueueNode planData = planDataOpt.get();
+
+            Assertions.assertEquals(21, planData.getDistance());
+            Assertions.assertEquals(21, planData.getMoves().size());
+        } catch (IOException e) {
+            Assertions.fail();
+        }
+    }
+
+    @DisplayName("AStar Test")
+    @Test
+    public void AStarTest() {
+        File file = new File("src/test/resources/maps/testmap2.txt");
+
+        if (!file.exists()) {
+            Assertions.fail("Resource not found");
+        }
+
+        String path = file.getAbsolutePath();
+
+        MapImporter importer = new MapImporter();
+
+        try {
+            importer.load(path);
+
+            Optional<Tile> spawntileOpt = Factory.getMapRepository().getBoard().getByCoordinates(2,2);
+
+            if (spawntileOpt.isEmpty()) {
+                Assertions.fail();
+            }
+
+            Tile spawnTile = spawntileOpt.get();
+
+            Guard guard = new Guard(spawnTile, Angle.RIGHT);
+            YamauchiAgent agent = new YamauchiAgent(guard);
+
+            HashMap<Integer, HashMap<Integer, Tile>> region = Factory.getMapRepository().getBoard().subset(0, 0, 10,10);
+
+            if (region.isEmpty()) {
+                Assertions.fail();
+            }
+
+            agent.addKnowledge(region);
+
+            Optional<Tile> targetOpt = Factory.getMapRepository().getBoard().getByCoordinates(5, 2);
+
+            if (targetOpt.isEmpty()) {
+                Assertions.fail();
+            }
+
+            Tile target = targetOpt.get();
+
+            Optional<QueueNode> planDataOpt = (new AStar()).execute(agent.getKnowledge(), agent.getPlayer(), target);
 
             if (planDataOpt.isEmpty()) {
                 Assertions.fail();

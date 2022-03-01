654c9838ec328f560ae438c9b26c4b33053c6ce4
Noah Scharrenberg <nscharrenberg@hotmail.com>
2022-03-01 14:39:08 +0100

    fix vision

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/CharacterVision.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/CharacterVision.java
@@ -138,7 +138,13 @@ public class CharacterVision{
             }
 
             if(validtile) {
-                finalvision.add(t);
+                Optional<Tile> tileAddOpt = board.getByCoordinates(t.getX(), t.getY());
+
+                if (tileAddOpt.isEmpty()) {
+                    continue;
+                }
+
+                finalvision.add(tileAddOpt.get());
             } else {
                 validtile = true;
             }

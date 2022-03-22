a8f77264a560dbacd3f5f35292d6090ec735e369
Dikaver <dika1596@gmail.com>
2022-03-22 10:06:59 +0100

    Add agent settings for the game configuration

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/recorder/GameConfigurationRecorder.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/utils/recorder/GameConfigurationRecorder.java
@@ -1,5 +1,8 @@
 package com.nscharrenberg.um.multiagentsurveillance.headless.utils.recorder;
+import com.nscharrenberg.um.multiagentsurveillance.agents.frontier.yamauchi.YamauchiAgent;
 import com.nscharrenberg.um.multiagentsurveillance.agents.shared.Agent;
+import com.nscharrenberg.um.multiagentsurveillance.agents.shared.algorithms.pathfinding.AStar.AStar;
+import com.nscharrenberg.um.multiagentsurveillance.agents.shared.algorithms.pathfinding.BFS.BFS;
 import com.nscharrenberg.um.multiagentsurveillance.headless.Factory;
 
 import com.google.gson.Gson;
@@ -8,6 +11,7 @@ import com.nscharrenberg.um.multiagentsurveillance.headless.utils.recorder.json.
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,9 +48,10 @@ public class GameConfigurationRecorder {
 
         Gson gson = new Gson();
 
-        Map<Integer, String> agents = new HashMap();
+        Map<Integer, List<String>> agents = new HashMap();
         for (int i = 0; i < agentList.size(); i++) {
-            agents.put(i, "Yamuchi,BFS"); //TODO: Grab this information from the object or GUI???
+            List<String> agentSetting = getAgentSettings(agentList.get(i));
+            agents.put(i, agentSetting);
         }
 
         GameJSON gameConf = new GameJSON(fileCount, agentList.size(), agents);
@@ -71,5 +76,21 @@ public class GameConfigurationRecorder {
                 throw new RuntimeException("Error Agents recorder in GameConfigurationRecorder.java");
             }
         }
+    }
+
+    private List<String> getAgentSettings(Agent agent){
+        List<String> agentSettings = new ArrayList<>();
+        if(agent instanceof YamauchiAgent){
+            agentSettings.add("Yamuchi");
+        }
+
+        if(agent.getPathFindingAlgorithm() instanceof BFS){
+            agentSettings.add("BFS");
+        } else if(agent.getPathFindingAlgorithm() instanceof AStar){
+            agentSettings.add("AStar");
+        } else if(agent.getPathFindingAlgorithm() == null){
+            agentSettings.add("null");
+        }
+        return agentSettings;
     }
 }

bfb8163b3714c7c7e929663e862fca2d24b81cad
Dikaver <dika1596@gmail.com>
2022-04-16 18:41:56 +0200

    Test Shadow Tiles

---
+++ src/test/java/headless/ShadowTileTest.java
@@ -0,0 +1,134 @@
+package headless;
+
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.Angle;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.Tile;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShadowTileTest {
+
+
+    @Test
+    void shadowUpBoundsLevel1(){
+        Tile tile = new Tile(10, 10);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.UP));
+    }
+
+    @Test
+    void shadowUpBoundsLevel2(){
+        Tile tile = new Tile(30, 9);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.UP));
+    }
+
+    @Test
+    void shadowUpBoundsLevel3(){
+        Tile tile = new Tile(30, 8);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertFalse(checkShadowTile(tile, xBound, yBound, Angle.UP));
+    }
+
+    @Test
+    void shadowDownBoundsLevel1(){
+        Tile tile = new Tile(32, 10);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.DOWN));
+    }
+
+    @Test
+    void shadowDownBoundsLevel2(){
+        Tile tile = new Tile(10, 11);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.DOWN));
+    }
+
+    @Test
+    void shadowDownBoundsLevel3(){
+        Tile tile = new Tile(10, 12);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertFalse(checkShadowTile(tile, xBound, yBound, Angle.DOWN));
+    }
+
+    @Test
+    void shadowLeftBoundsLevel1(){
+        Tile tile = new Tile(10, 54);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.LEFT));
+    }
+
+    @Test
+    void shadowLeftBoundsLevel2(){
+        Tile tile = new Tile(9, 10);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.LEFT));
+    }
+
+    @Test
+    void shadowLeftBoundsLevel3(){
+        Tile tile = new Tile(8, 65);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertFalse(checkShadowTile(tile, xBound, yBound, Angle.LEFT));
+    }
+
+    @Test
+    void shadowRightBoundsLevel1(){
+        Tile tile = new Tile(10, 10);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.RIGHT));
+    }
+
+    @Test
+    void shadowRightBoundsLevel2(){
+        Tile tile = new Tile(11, 65);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertTrue(checkShadowTile(tile, xBound, yBound, Angle.RIGHT));
+    }
+
+    @Test
+    void shadowRightBoundsLevel3(){
+        Tile tile = new Tile(12, 65);
+        int xBound = 10;
+        int yBound = 10;
+        Assertions.assertFalse(checkShadowTile(tile, xBound, yBound, Angle.RIGHT));
+    }
+
+
+
+
+    private boolean checkShadowTile(Tile tile, int xBound, int yBound, Angle direction){
+
+        int shadowBound = 1;
+
+        if(direction == Angle.UP){
+
+            return tile.getY() >= yBound - shadowBound;
+
+        } else if(direction == Angle.DOWN){
+
+            return tile.getY() <= yBound + shadowBound;
+
+        } else if(direction == Angle.LEFT){
+
+            return tile.getX() >= xBound - shadowBound;
+
+        } else if(direction == Angle.RIGHT){
+
+            return tile.getX() <= xBound + shadowBound;
+
+        }
+
+        return false;
+    }
+}

347e909e9a85564923f5d2ec3ae5852937faa520
XPC <43468812+XPC1995@users.noreply.github.com>
2022-04-25 15:41:01 +0200

    Update Intruder.java

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
@@ -56,3 +56,72 @@ public class Intruder extends Player {
         return Objects.hash(super.hashCode(), sprintSpeed, isSprinting);
     }
 }
+
+
+   /**
+     * To make sure the intruder knows the direction of the target area
+     * @param position - the position of the intruder
+     * @return angle - the angle between the intruder and the target area
+     * @throws RuntimeException - Thrown when there is no target direction provided for the intruder
+     */
+    public AdvancedAngle targetDirection(Tile position)
+    {
+        if (position == null) {
+            throw new RuntimeException("no intruder position provided for intruder");
+        }
+
+        // Upper Left
+        int upperLeftX = 20;
+        int upperLeftY = 40;
+
+        // Bottom Left
+        int bottomLeftX = 20;
+        int bottomLeftY = 45;
+
+        // Upper Right
+        int upperRightX = 25;
+        int upperRightY = 40;
+
+        // Bottom Right
+        int bottomRightX = 25;
+        int bottomRightY = 45;
+
+        Tile targetCentre = new Tile((upperLeftX + bottomRightX)/2, (upperLeftY + bottomRightY)/2);
+
+        int dX = targetCentre.getX() - position.getX();
+        int dY = targetCentre.getY() - position.getY();
+
+        // Normalization
+        dX = Integer.compare(dX, 0);
+        dY = Integer.compare(dY, 0);
+
+        int pick;
+        if(dX == 0 && dY == -1)
+            pick = 0;
+        else if(dX == 0 && dY == 1)
+            pick = 1;
+        else if(dX == -1 && dY ==0)
+            pick = 2;
+        else if(dX == 1 && dY == 0)
+            pick = 3;
+        else if(dX == -1 && dY == -1)
+            pick = 4;
+        else if(dX == 1 && dY == -1)
+            pick = 5;
+        else if(dX == -1 && dY == 1)
+            pick = 6;
+        else
+            pick = 7;
+
+        AdvancedAngle targetDirection = AdvancedAngle.values()[pick];
+
+        if(targetDirection == null)
+        {
+            throw new RuntimeException("no targetDirection provided for intruder");
+        }
+
+        return targetDirection;
+    }
+
+
+}

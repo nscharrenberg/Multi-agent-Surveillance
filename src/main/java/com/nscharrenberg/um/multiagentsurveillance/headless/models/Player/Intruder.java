8c2e7a731fbe11f2fff838fabc11247cf58d0a25
XPC <43468812+XPC1995@users.noreply.github.com>
2022-04-27 06:49:26 +0200

    Update Intruder.java

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
@@ -1,13 +1,18 @@
 package com.nscharrenberg.um.multiagentsurveillance.headless.models.Player;
 
+import com.nscharrenberg.um.multiagentsurveillance.headless.Factory;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.AdvancedAngle;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.Angle;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.Tile;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.TileArea;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Intruder extends Player {
     private double sprintSpeed;
     private boolean isSprinting = false;
+    private Angle targetDirection;
 
     public Intruder(Tile position, Angle direction) {
         // TODO: Read speed from Configuration
@@ -15,6 +20,8 @@ public class Intruder extends Player {
 
         // TODO: Read sprint speed from Configuration
         this.sprintSpeed = 20;
+
+        targetDirection = computeTargetDirection(position.getX(), position.getY());
     }
 
     public double getSprintSpeed() {
@@ -55,4 +62,71 @@ public class Intruder extends Player {
     public int hashCode() {
         return Objects.hash(super.hashCode(), sprintSpeed, isSprinting);
     }
+
+    public Angle computeTargetDirection(int x1, int y1) {
+
+        TileArea targetArea = Factory.getMapRepository().getTargetArea();
+        List<Tile> bounds = targetArea.getBounds();
+
+        if (bounds.isEmpty()) {
+            throw new RuntimeException("no target position provided for the intruder");
+        }
+
+        // Top Left
+        int topLeftX = bounds.get(0).getX();
+        int topLeftY = bounds.get(0).getY();
+
+        // Bottom Right
+        int bottomRightX = bounds.get(3).getX();
+        int bottomRightY = bounds.get(3).getY();
+
+        Tile targetCentre = new Tile((topLeftX + bottomRightX)/2, (topLeftY + bottomRightY)/2);
+
+        int x2 = targetCentre.getX();
+        int y2 = targetCentre.getY();
+
+        int x = x2 - x1;
+        int y = y2 - y1;
+
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+
+        if(x == 0 && y == 0)
+            throw new RuntimeException("Target position Error");
+
+
+        if(x == 0){
+
+            if(y > 0) return Angle.DOWN;
+            else return Angle.UP;
+
+        } else if(y == 0){
+
+            if(x > 0) return Angle.RIGHT;
+            else return Angle.LEFT;
+
+        } else if(x > 0){
+
+            if(y > 0) {
+                if (absX > absY) return Angle.RIGHT;
+                else return Angle.DOWN;
+            } else {
+                if(absX > absY) return Angle.RIGHT;
+                else return Angle.UP;
+            }
+
+        } else {
+
+            if(y > 0) {
+                if (absX > absY) return Angle.LEFT;
+                else return Angle.DOWN;
+            } else {
+                if(absX > absY) return Angle.LEFT;
+                else return Angle.UP;
+            }
+
+        }
+    }
+
+
 }

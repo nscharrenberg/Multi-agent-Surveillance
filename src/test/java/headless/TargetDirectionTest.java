63dfa6f138ffe6b47769808c8b6539c4c59b5952
XPC <43468812+XPC1995@users.noreply.github.com>
2022-04-27 06:47:54 +0200

    Update TargetDirectionTest.java

--- src/test/java/headless/TargetDirectionTest.java
+++ src/test/java/headless/TargetDirectionTest.java
@@ -1,12 +1,18 @@
 package headless;
 
 import com.nscharrenberg.um.multiagentsurveillance.headless.Factory;
+import com.nscharrenberg.um.multiagentsurveillance.headless.exceptions.*;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.AdvancedAngle;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.Angle;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.Tile;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.TileArea;
+import com.nscharrenberg.um.multiagentsurveillance.headless.utils.files.MapImporter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Stack;
 
@@ -16,122 +22,115 @@ public class TargetDirectionTest {
     @Test
     public void test1(){
         // Test if the target direction is computed correctly
-        Tile pos = new Tile(22, 80);
-        Assertions.assertEquals("UP", targetDirection(pos).name());
+        Tile pos = new Tile(5, 10);
+        Assertions.assertEquals("UP", computeTargetDirection(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("DOWN")
     @Test
     public void test2(){
         // Test if the target direction is computed correctly
-        Tile pos = new Tile(22, -50);
-        Assertions.assertEquals("DOWN", targetDirection(pos).name());
+        Tile pos = new Tile(5, 2);
+        Assertions.assertEquals("DOWN", computeTargetDirection(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("LEFT")
     @Test
     public void test3(){
         // Test if the target direction is computed correctly
-        Tile pos = new Tile(50, 42);
-        Assertions.assertEquals("LEFT", targetDirection(pos).name());
+        Tile pos = new Tile(10, 4);
+        Assertions.assertEquals("LEFT", computeTargetDirection(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("RIGHT")
     @Test
     public void test4(){
         // Test if the target direction is computed correctly
-        Tile pos = new Tile(-45, 42);
-        Assertions.assertEquals("RIGHT", targetDirection(pos).name());
+        Tile pos = new Tile(0, 2);
+        Assertions.assertEquals("RIGHT", computeTargetDirection(pos.getX(), pos.getY()).name());
     }
 
-    @DisplayName("TOP_LEFT")
-    @Test
-    public void test5(){
-        // Test if the target direction is computed correctly
-        Tile pos = new Tile(75, 75);
-        Assertions.assertEquals("TOP_LEFT", targetDirection(pos).name());
-    }
 
-    @DisplayName("TOP_RIGHT")
-    @Test
-    public void test6(){
-        // Test if the target direction is computed correctly
-        Tile pos = new Tile(-20, 75);
-        Assertions.assertEquals("TOP_RIGHT", targetDirection(pos).name());
-    }
+    public Angle computeTargetDirection(int x1, int y1) {
 
-    @DisplayName("BOTTOM_LEFT")
-    @Test
-    public void test7(){
-        // Test if the target direction is computed correctly
-        Tile pos = new Tile(50, -30);
-        Assertions.assertEquals("BOTTOM_LEFT", targetDirection(pos).name());
-    }
+        File file = new File("src/test/resources/maps/testmap6.txt");
 
-    @DisplayName("BOTTOM_RIGHT")
-    @Test
-    public void test8(){
-        // Test if the target direction is computed correctly
-        Tile pos = new Tile(50, -40);
-        Assertions.assertEquals("BOTTOM_LEFT", targetDirection(pos).name());
-    }
-
-    public AdvancedAngle targetDirection(Tile position)
-    {
-        if (position == null) {
-            throw new RuntimeException("no intruder position provided for intruder");
+        if (!file.exists()) {
+            Assertions.fail("Resource not found");
         }
-        
-        // Upper Left
-        int upperLeftX = 20;
-        int upperLeftY = 40;
 
-        // Bottom Left
-        int bottomLeftX = 20;
-        int bottomLeftY = 45;
+        String path = file.getAbsolutePath();
 
-        // Upper Right
-        int upperRightX = 25;
-        int upperRightY = 40;
+        MapImporter importer = new MapImporter();
+
+        try {
+            importer.load(path);
+
+        } catch (IOException e) {
+        Assertions.fail();
+        }
+
+        TileArea targetArea = Factory.getMapRepository().getTargetArea();
+        List<Tile> bounds = targetArea.getBounds();
+
+        if (bounds.isEmpty()) {
+            throw new RuntimeException("no target position provided for the intruder");
+        }
+
+        // Top Left
+        int topLeftX = bounds.get(0).getX();
+        int topLeftY = bounds.get(0).getY();
 
         // Bottom Right
-        int bottomRightX = 25;
-        int bottomRightY = 45;
+        int bottomRightX = bounds.get(3).getX();
+        int bottomRightY = bounds.get(3).getY();
 
-        Tile targetCentre = new Tile((upperLeftX + bottomRightX)/2, (upperLeftY + bottomRightY)/2);
+        Tile targetCentre = new Tile((topLeftX + bottomRightX)/2, (topLeftY + bottomRightY)/2);
 
-        int dX = targetCentre.getX() - position.getX();
-        int dY = targetCentre.getY() - position.getY();
+        int x2 = targetCentre.getX();
+        int y2 = targetCentre.getY();
 
-        // Normalization
-        dX = Integer.compare(dX, 0);
-        dY = Integer.compare(dY, 0);
+        int x = x2 - x1;
+        int y = y2 - y1;
 
-        int pick;
-        if(dX == 0 && dY == -1)
-            pick = 0;
-        else if(dX == 0 && dY == 1)
-            pick = 1;
-        else if(dX == -1 && dY ==0)
-            pick = 2;
-        else if(dX == 1 && dY == 0)
-            pick = 3;
-        else if(dX == -1 && dY == -1)
-            pick = 4;
-        else if(dX == 1 && dY == -1)
-            pick = 5;
-        else if(dX == -1 && dY == 1)
-            pick = 6;
-        else
-            pick = 7;
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
 
-        AdvancedAngle targetDirection = AdvancedAngle.values()[pick];
+        if(x == 0 && y == 0)
+            throw new RuntimeException("Target position Error");
 
-        if(targetDirection == null)
-        {
-            throw new RuntimeException("no targetDirection provided for intruder");
+
+        if(x == 0){
+
+            if(y > 0) return Angle.DOWN;
+            else return Angle.UP;
+
+        } else if(y == 0){
+
+            if(x > 0) return Angle.RIGHT;
+            else return Angle.LEFT;
+
+        } else if(x > 0){
+
+            if(y > 0) {
+                if (absX > absY) return Angle.RIGHT;
+                else return Angle.DOWN;
+            } else {
+                if(absX > absY) return Angle.RIGHT;
+                else return Angle.UP;
+            }
+
+        } else {
+
+            if(y > 0) {
+                if (absX > absY) return Angle.LEFT;
+                else return Angle.DOWN;
+            } else {
+                if(absX > absY) return Angle.LEFT;
+                else return Angle.UP;
+            }
+
         }
-
-        return targetDirection;
     }
+
 }

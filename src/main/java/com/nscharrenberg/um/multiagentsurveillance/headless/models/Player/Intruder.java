17c5d52cb28aa22e3c7b41d0d82738f187773a12
XPC <43468812+XPC1995@users.noreply.github.com>
2022-04-28 20:07:06 +0200

    Update Intruder.java

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
@@ -5,7 +5,11 @@ import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.Advance
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.Angle;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.Tile;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.TileArea;
+import com.nscharrenberg.um.multiagentsurveillance.headless.utils.files.MapImporter;
+import com.nscharrenberg.um.multiagentsurveillance.headless.utils.TargetDirection;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,7 +25,7 @@ public class Intruder extends Player {
         // TODO: Read sprint speed from Configuration
         this.sprintSpeed = 20;
 
-        targetDirection = computeTargetDirection(position.getX(), position.getY());
+        targetDirection = TargetDirection.computeTargetDirection(position.getX(), position.getY());
     }
 
     public double getSprintSpeed() {
@@ -63,87 +67,7 @@ public class Intruder extends Player {
         return Objects.hash(super.hashCode(), sprintSpeed, isSprinting);
     }
 
-    public Angle computeTargetDirection(int x1, int y1) {
-        
-        File file = new File("src/test/resources/maps/exam.txt");
 
-        if (!file.exists()) {
-            throw new RuntimeException("Resource not found");
-        }
-
-        String path = file.getAbsolutePath();
-
-        MapImporter importer = new MapImporter();
-
-        try {
-            importer.load(path);
-
-        } catch (IOException e) {
-            throw new RuntimeException("Importer failed");
-        }
-
-        TileArea targetArea = Factory.getMapRepository().getTargetArea();
-        List<Tile> bounds = targetArea.getBounds();
-
-        if (bounds.isEmpty()) {
-            throw new RuntimeException("no target position provided for the intruder");
-        }
-
-        // Top Left
-        int topLeftX = bounds.get(0).getX();
-        int topLeftY = bounds.get(0).getY();
-
-        // Bottom Right
-        int bottomRightX = bounds.get(3).getX();
-        int bottomRightY = bounds.get(3).getY();
-
-        Tile targetCentre = new Tile((topLeftX + bottomRightX)/2, (topLeftY + bottomRightY)/2);
-
-        int x2 = targetCentre.getX();
-        int y2 = targetCentre.getY();
-
-        int x = x2 - x1;
-        int y = y2 - y1;
-
-        int absX = Math.abs(x);
-        int absY = Math.abs(y);
-
-        if(x == 0 && y == 0)
-            throw new RuntimeException("Target position Error");
-
-
-        if(x == 0){
-
-            if(y > 0) return Angle.DOWN;
-            else return Angle.UP;
-
-        } else if(y == 0){
-
-            if(x > 0) return Angle.RIGHT;
-            else return Angle.LEFT;
-
-        } else if(x > 0){
-
-            if(y > 0) {
-                if (absX > absY) return Angle.RIGHT;
-                else return Angle.DOWN;
-            } else {
-                if(absX > absY) return Angle.RIGHT;
-                else return Angle.UP;
-            }
-
-        } else {
-
-            if(y > 0) {
-                if (absX > absY) return Angle.LEFT;
-                else return Angle.DOWN;
-            } else {
-                if(absX > absY) return Angle.LEFT;
-                else return Angle.UP;
-            }
-
-        }
-    }
 
 
 }

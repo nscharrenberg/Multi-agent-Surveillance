056a0fa7af5c8a250103ae97c091b6e89077f17a
XPC <43468812+XPC1995@users.noreply.github.com>
2022-05-17 16:22:25 +0200

    Changed to latest version

--- src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
+++ src/main/java/com/nscharrenberg/um/multiagentsurveillance/headless/models/Player/Intruder.java
@@ -1,31 +1,54 @@
 package com.nscharrenberg.um.multiagentsurveillance.headless.models.Player;
 
+import com.nscharrenberg.um.multiagentsurveillance.agents.shared.algorithms.distanceCalculator.CalculateDistance;
+import com.nscharrenberg.um.multiagentsurveillance.agents.shared.algorithms.distanceCalculator.ManhattanDistance;
 import com.nscharrenberg.um.multiagentsurveillance.headless.Factory;
-import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.AdvancedAngle;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Action;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.Tile;
-import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.TileArea;
-import com.nscharrenberg.um.multiagentsurveillance.headless.utils.files.MapImporter;
-import com.nscharrenberg.um.multiagentsurveillance.headless.utils.TargetDirection;
+import com.nscharrenberg.um.multiagentsurveillance.headless.utils.targetPositionCalculator.TargetPositionCalculator;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 public class Intruder extends Player {
     private double sprintSpeed;
     private boolean isSprinting = false;
-    private AdvancedAngle targetDirection;
 
-    public Intruder(Tile position, AdvancedAngle direction) {
+    private Action targetAngle;
+    private double distanceToTarget = Double.MAX_VALUE;
+    private Tile target;
+    private final TargetPositionCalculator targetPositionCalculator;
+    private final CalculateDistance calculateDistance = new ManhattanDistance();
+
+    public Intruder(Tile position, Action direction) {
         // TODO: Read speed from Configuration
         super(position, direction, 10);
 
+        this.targetPositionCalculator = new TargetPositionCalculator();
         // TODO: Read sprint speed from Configuration
         this.sprintSpeed = 20;
+    }
 
-        // this.targetDirection = TargetDirection.computeTargetDirection(position.getX(), position.getY());
-        this.targetDirection = TargetDirection.computeTargetDirectionInAdvancedAngle(position.getX(), position.getY());
+    public Action getTargetAngle() {
+        return targetAngle;
+    }
+
+    public double getDistanceToTarget(){
+        return this.distanceToTarget;
+    }
+
+    public Tile getTarget(){
+        return this.target;
+    }
+
+    public void updateTargetInfo() {
+        this.targetAngle = Factory.getGameRepository().getTargetGameAngle(this);
+
+        if(this.target == null) {
+            this.target = targetPositionCalculator.calculate(Factory.getGameRepository().getTargetRealAngle(this),
+                    getTile());
+        } else {
+            this.distanceToTarget = this.calculateDistance.compute(this.target, getTile());
+        }
     }
 
     public double getSprintSpeed() {
@@ -66,10 +89,4 @@ public class Intruder extends Player {
     public int hashCode() {
         return Objects.hash(super.hashCode(), sprintSpeed, isSprinting);
     }
-
-    public AdvancedAngle getTargetDirection()
-    {
-        return targetDirection;
-    }
-    
 }

1839f6bc22623edc81c15ef51f849189d55bbf4e
XPC <43468812+XPC1995@users.noreply.github.com>
2022-05-17 12:55:41 +0200

    Update TargetDirectionTest.java

--- src/test/java/headless/TargetDirectionTest.java
+++ src/test/java/headless/TargetDirectionTest.java
@@ -1,10 +1,16 @@
 package headless;
 
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.AdvancedAngle;
+import com.nscharrenberg.um.multiagentsurveillance.headless.models.Angle.Angle;
 import com.nscharrenberg.um.multiagentsurveillance.headless.models.Map.Tile;
 import com.nscharrenberg.um.multiagentsurveillance.headless.utils.TargetDirection;
+import com.nscharrenberg.um.multiagentsurveillance.headless.utils.files.MapImporter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class TargetDirectionTest {
@@ -15,7 +21,7 @@ public class TargetDirectionTest {
     public void test1(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(5, 10);
-        Assertions.assertEquals("UP", TargetDirection.computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("UP", computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("DOWN")
@@ -23,7 +29,7 @@ public class TargetDirectionTest {
     public void test2(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(5, 2);
-        Assertions.assertEquals("DOWN", TargetDirection.computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("DOWN", computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("LEFT")
@@ -31,7 +37,7 @@ public class TargetDirectionTest {
     public void test3(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(10, 4);
-        Assertions.assertEquals("LEFT", TargetDirection.computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("LEFT", computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("RIGHT")
@@ -39,7 +45,7 @@ public class TargetDirectionTest {
     public void test4(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(0, 2);
-        Assertions.assertEquals("RIGHT", TargetDirection.computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("RIGHT", computeTargetDirectionTesting(pos.getX(), pos.getY()).name());
     }
 
     // Test target direction in ADVANCED ANGLE
@@ -49,7 +55,7 @@ public class TargetDirectionTest {
     public void test5(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(5, 4);
-        Assertions.assertEquals("UP", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("UP", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("DOWN")
@@ -57,7 +63,7 @@ public class TargetDirectionTest {
     public void test6(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(5, 2);
-        Assertions.assertEquals("DOWN", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("DOWN", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("LEFT")
@@ -65,7 +71,7 @@ public class TargetDirectionTest {
     public void test7(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(6, 4);
-        Assertions.assertEquals("LEFT", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("LEFT", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("RIGHT")
@@ -73,7 +79,7 @@ public class TargetDirectionTest {
     public void test8(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(4, 4);
-        Assertions.assertEquals("RIGHT", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("RIGHT", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("BOTTOM_LEFT")
@@ -81,7 +87,7 @@ public class TargetDirectionTest {
     public void test9(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(10, 2);
-        Assertions.assertEquals("BOTTOM_LEFT", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("BOTTOM_LEFT", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("TOP_LEFT")
@@ -89,7 +95,7 @@ public class TargetDirectionTest {
     public void test10(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(10, 6);
-        Assertions.assertEquals("TOP_LEFT", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("TOP_LEFT", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("BOTTOM_RIGHT")
@@ -97,7 +103,7 @@ public class TargetDirectionTest {
     public void test11(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(2, 2);
-        Assertions.assertEquals("BOTTOM_RIGHT", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("BOTTOM_RIGHT", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
     @DisplayName("TOP_RIGHT")
@@ -105,8 +111,52 @@ public class TargetDirectionTest {
     public void test12(){
         // Test if the target direction is computed correctly
         Tile pos = new Tile(2, 10);
-        Assertions.assertEquals("TOP_RIGHT", TargetDirection.computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
+        Assertions.assertEquals("TOP_RIGHT", computeTargetDirectionInAdvancedAngleTesting(pos.getX(), pos.getY()).name());
     }
 
-}
+    public static Angle computeTargetDirectionTesting(int x1, int y1) {
 
+        File file = new File("src/test/resources/maps/testmap6.txt");
+
+        if (!file.exists()) {
+            throw new RuntimeException("Resource not found");
+        }
+
+        String path = file.getAbsolutePath();
+
+        MapImporter importer = new MapImporter();
+
+        try {
+            importer.load(path);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Importer failed");
+        }
+
+        return TargetDirection.computeTargetDirection(x1, y1);
+
+    }
+
+    public static AdvancedAngle computeTargetDirectionInAdvancedAngleTesting(int x, int y) {
+
+        File file = new File("src/test/resources/maps/testmap6.txt");
+
+        if (!file.exists()) {
+            throw new RuntimeException("Resource not found");
+        }
+
+        String path = file.getAbsolutePath();
+
+        MapImporter importer = new MapImporter();
+
+        try {
+            importer.load(path);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Importer failed");
+        }
+
+        return TargetDirection.computeTargetDirectionInAdvancedAngle(x, y);
+
+    }
+}
